dd9ffffe7e45a30eb427d51a482153aba2ecbae4
pedro <pedro.braga@maxmilhas.com.br>
2017-08-28 12:56:22 -0300

    all redux fases working

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-
-import Booklist from '../containers/book_list'
+import Booklist from '../containers/book_list';
+import BookDetail from '../containers/book_detail';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Booklist />
+        <BookDetail />
       </div>
     );
   }
